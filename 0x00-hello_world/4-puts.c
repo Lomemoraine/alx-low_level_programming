2e424a850ace7d11ed661a3afbbb33166d7d2800
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-20 13:15:53 -0700

    resume after sleep

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point that will return aa void
+ *
+ *Return: Always  returns 0 for a successful execution
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
