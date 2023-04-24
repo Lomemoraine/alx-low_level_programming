dfb854ca50c63b7525c0ee1c90e1306c12b4ab32
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-24 14:29:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code execution
+ *
+ * Return:0 for a succeful execution
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
