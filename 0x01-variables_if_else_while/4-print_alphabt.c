1d873db2f380692f82610248343e2ad9cfd9009c
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-24 14:54:17 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - Entry for code execution
+ *
+ *Return:0 for a succesful compilation
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
