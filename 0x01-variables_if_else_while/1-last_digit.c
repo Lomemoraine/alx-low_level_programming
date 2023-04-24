6ee244e1e35713b9f875d734c47c7e91ee4bdb04
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-24 14:21:15 -0700

    add time library to task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+#include  <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -8,7 +9,7 @@ int main(void)
 {
 	int n, n1;
 
-	
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
 	if (nl > 5)
