37d2d5a2c9f1459b265e9b915130c550605df3b7
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-28 16:56:59 -0700

    print remaing days correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,8 +2,11 @@
 #include "main.h"
 
 /**
-  * print_remaining_days - takes three arguments,month,day and year and calculates the reamining days
-  * 
+  * print_remaining_days - takes a date and prints how many days are
+  * left in the year, taking leap years into account
+  * @month: month in number format
+  * @day: day of month
+  * @year: year
   * Return: void
   */
 
