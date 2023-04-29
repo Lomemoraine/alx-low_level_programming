eddb271c0de44b66e0336e43b412b9b24a2b57b4
Lomemoraine <lorrainechepkemoi4@gmail.com>
2023-04-28 17:56:13 -0700

    multiplication

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *print_numbers - prints the numbers, from 0 to 9
- *Return: The numbers from 0 to 10
- */
-
+  * print_numbers - Print the numbers from 0 to 9
+  *
+  * Return: The numbers from 0  to 9
+  */
 void print_numbers(void)
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	putchar(x + '0');
+		_putchar(x + '0');
 	}
 
 	_putchar('\n');
